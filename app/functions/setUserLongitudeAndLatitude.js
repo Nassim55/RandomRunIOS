bf663bf85e239e7f1b5663f2fa9d81556d6bd5c0
Nassim55 <nassim.chenouf@outlook.com>
2020-09-26 10:50:08 +0100

    Location request now works on ios and Android

--- app/functions/setUserLongitudeAndLatitude.js
+++ app/functions/setUserLongitudeAndLatitude.js
@@ -4,6 +4,7 @@ import { isLocationPermissionGranted, setUserLocation } from '../../store/action
 
 const setUserLongitudeAndLatitude = async (dispatch) => {
   if (Platform.OS === 'android') {
+    // Checking to see if the Android location permission is granted:
     try {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION, {
@@ -14,27 +15,34 @@ const setUserLongitudeAndLatitude = async (dispatch) => {
           buttonPositive: "OK"
         }
       );
+
+      // If the Android location permission is granted, get the users location and update state:
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         dispatch(isLocationPermissionGranted(true));
-        try {
-            Geolocation.getCurrentPosition(position => {
-              dispatch(setUserLocation([position.coords.longitude, position.coords.latitude]));
-            }, 
-            err => console.error(err),
-            { enableHighAccuracy: true, timeout: 5000, maximumAge: 0, }
-            );
-        } catch (err) { if (console) console.error(err) };
-      } else {
+        Geolocation.getCurrentPosition(position => {
+          dispatch(setUserLocation([position.coords.longitude, position.coords.latitude]));
+        }, 
+        err => console.error(err),
+        {enableHighAccuracy: true, timeout: 60000, maximumAge: 0}
+        );
+      } else { 
         dispatch(isLocationPermissionGranted(false));
       };
-    } catch (err) {
-      if (console) {
-        console.error(err);
-      };
-    };
+
+    } catch (err) { if (console) console.error(err) };
+
   } else if (Platform.OS === 'ios') {
-    dispatch(isLocationPermissionGranted(true));
-    dispatch(setUserLocation([-1.6178, 54.9783]));
+    // Sets IOS permissions and upadates the user location in state accordingly:
+    Geolocation.getCurrentPosition(position => {
+      dispatch(isLocationPermissionGranted(true));
+      dispatch(setUserLocation([position.coords.longitude, position.coords.latitude]));
+      }, 
+      err => {
+        dispatch(isLocationPermissionGranted(false));
+        console.error(err);
+      },
+      {enableHighAccuracy: true, timeout: 60000, maximumAge: 0}
+    );
   }
 };
 
